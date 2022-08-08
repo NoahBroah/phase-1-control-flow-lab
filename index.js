54686abed9e6bb85f59994a52ea93aec421790eb
Noah King <noahking123@yahoo.com>
2022-08-08 13:17:09 -0400

    commit tree 5fa3b348ba1d76c02efd27ac7e20f9bc7c3c20b5

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someNumber){
+  let greeting;
+  if (someNumber < 400) {
+    greeting = "This one is on me!";
+    return greeting;
+  } else if (someNumber > 400 && someNumber < 2000) {
+    return 'That will be twenty bucks.'
+  } else if (someNumber > 2000 && someNumber < 2500) {
+    return 'I will gladly take your thirty bucks.'
+  } else if (someNumber > 2500) {
+    return 'No can do.'
+  }
+  
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let destination;
+  city === 'NYC' ? (destination = "Ok, sounds good.") : (destination = 'No go.');
+  return destination;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let response;
+  switch(tip){
+    case 'generous':
+      response = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      response = 'Thank you.';
+      break;
+    default:
+      response = 'Bye.'
+  }
+  return response;
 }
